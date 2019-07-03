80360b2585f27eda5bb93eaa8544cf9081f61cbf
pkasela <p.kasela@campus.unimib.it>
2019-07-03 23:28:47 +0200

    Riaggiustato le cartelle e tutto

--- source.R
+++ source.R
@@ -1,23 +1,23 @@
 setwd('/home/pranav/Desktop/Web-Marketing-Project/')
 
 #Script preparation as done in class
-source('script_preparation.R')
-#model using DT
-source('Email_Engagement_DT.R')
+source('Email_Engagement/script_preparation.R')
+#model using DT and RF
+source('Email_Engagement/Email_Engagement_DT.R')
 #model using RF
-source('Email_Engagement_Propensity.R')
+source('Email_Engagement/Email_Engagement_Propensity.R')
 #model using xgBOOST
-source('Email_Engagement_Propensity_xgBOOST.R')
+source('Email_Engagement/Email_Engagement_Propensity_xgBOOST.R')
 #plot accuracy
 
 #Script prepartion for the churners
-source('Creazione_churner.R')
-#model using DT
-source('churn_DT.R')
+source('Propensity_churn/Creazione_churner.R')
+#model using DT and RF
+source('Propensity_churn/churn_DT.R')
 #model using RF
-source('churn_RF.R')
+source('Propensity_churn/churn_RF.R')
 #model using xgBOOST
-source('churn_xgb.R')
+source('Propensity_churn/churn_xgb.R')
 
 #model to predict the future income using ARIMA and LSTM (RNN)
-source('time_Series.R')
+source('Time_Series_Model/time_Series.R')
